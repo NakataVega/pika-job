ece9f8f70086b7d5851a76fccebc5b145e8c3f26
Daniel Ortega <dani.ortega9607@gmail.com>
2020-03-03 09:48:36 -0600

    Enable db on prod and dev

--- knexfile.js
+++ knexfile.js
@@ -20,20 +20,5 @@ module.exports = {
     }
   },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+  production: app.get('mysql'),
 };
